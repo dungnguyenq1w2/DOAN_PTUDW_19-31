2cb0e4806b7b2ce1408e252cdfdac9a495d599d7
t4n-du4ng <dungnguyenq1w2@gmail.com>
2021-11-21 19:56:31 +0700

    black background of page title on header__bottom

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Cake" });
+  res.render("index", { title: "Cake", home: "true" });
 });
 
 router.get("/signIn", function (req, res, next) {
-  res.render("signIn", { title: "Sign In" });
+  res.render("signIn", { title: "Sign In", signIn: "true" });
 });
 
 router.get("/about", (req, res, next) => {
-  res.render("about", { title: "About", path: "about" });
+  res.render("about", { title: "About", about: "true" });
 });
 
 router.get("/contact", (req, res, next) => {
-  res.render("contact", { title: "Contact" });
+  res.render("contact", { title: "Contact", contact: "true" });
 });
 
 router.get("/shop", (req, res, next) => {
-  res.render("shop", { title: "Shop" });
+  res.render("shop", { title: "Shop", shop: "true" });
 });
 
 router.get("/checkout", (req, res, next) => {
-  res.render("checkout", { title: "Checkout" });
+  res.render("checkout", { title: "Checkout", checkout: "true" });
 });
 
 router.get("/shoppingCart", (req, res, next) => {
