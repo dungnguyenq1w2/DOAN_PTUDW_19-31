181dea42df7b2c9338de76122ea2c45d61d0a422
t4n-du4ng <dungnguyenq1w2@gmail.com>
2022-01-03 19:43:18 +0700

    done statistics by period UI

--- admin/middlewares/passport.middleware.js
+++ admin/middlewares/passport.middleware.js
@@ -1,42 +1,45 @@
-const passport = require('passport');
-const { Strategy } = require('passport-local');
+const passport = require("passport");
+const { Strategy } = require("passport-local");
 
-const authService = require('../services/auth.service');
+const authService = require("../services/auth.service");
 
-passport.use(new Strategy({
-    usernameField: 'email',
-    passwordField: 'password'
-  },
-  async (username, password, done) => {
-    try {
-      const user = await authService.getRetrieveUserByEmail(username);
+passport.use(
+  new Strategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (username, password, done) => {
+      try {
+        const user = await authService.getRetrieveUserByEmail(username);
 
-      if (!user) {
-        const result = { message: 'Incorrect email' };
-        console.log(result.message);
-        return done(null, false, result);
+        if (!user) {
+          const result = { message: "Incorrect email" };
+          console.log(result.message);
+          return done(null, false, result);
+        }
+
+        if (!user.roles.includes("admin")) {
+          const result = { message: "No privilege" };
+          console.log(result.message);
+          return done(null, false, result);
+        }
+
+        const passwordMatch = await user.comparePassword(password);
+        if (!passwordMatch) {
+          const result = { message: "Incorrect password" };
+          console.log(result);
+          return done(null, false, result);
+        }
+
+        return done(null, user);
+      } catch (error) {
+        console.log(error);
+        return done(error);
       }
-
-      if (!user.roles.includes('admin')) {
-        const result = { message: 'No privilege' };
-        console.log(result.message);
-        return done(null, false, result);
-      }
-
-      const passwordMatch = await user.comparePassword(password);
-      if (!passwordMatch) {
-        const result = { message: 'Incorrect password' };
-        console.log(result);
-        return done(null, false, result);
-      }
-
-      return done(null, user);
-    } catch (error) {
-      console.log(error);
-      return done(error);
     }
-  }
-));
+  )
+);
 
 passport.serializeUser((user, done) => {
   done(null, user._id);
