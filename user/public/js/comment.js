5cd43d435c164cfadd799faa9c4b08113582b6e6
t4n-du4ng <dungnguyenq1w2@gmail.com>
2022-01-13 22:59:33 +0700

    update

--- user/public/js/comment.js
+++ user/public/js/comment.js
@@ -1,12 +1,12 @@
-const commentsList = document.getElementById('comments');
-const commentInput = document.getElementById('comment-input');
-const paginationBar = document.getElementById('pagination-bar');
+const commentsList = document.getElementById("comments");
+const commentInput = document.getElementById("comment-input");
+const paginationBar = document.getElementById("pagination-bar");
 
 const createElement = (literal) => {
-  const template = document.createElement('template');
+  const template = document.createElement("template");
   template.innerHTML = literal;
   return template.content.childNodes;
-}
+};
 
 const renderComments = (comments) => {
   commentsList.replaceChildren();
@@ -14,65 +14,62 @@ const renderComments = (comments) => {
   for (const comment of comments) {
     const [commentTag] = createElement(
       `<div class="comment__box">
+      <img src="${comment.user.avatar}" alt="" />
+      <div class="comment__content">
       <div class="comment__customer__info">
-        <img src="${comment.user.avatar}" alt="" />
-        <div>
           <h4>${comment.user.name}</h4>
-          <span>${(new Date(comment.createdAt)).toLocaleString()}</span>
-        </div>
+          <span>${new Date(comment.createdAt).toLocaleString()}</span>
       </div>
       <div class="comment__message">
         <p>${comment.content}</p>
+      </div>
       </div>
     </div>`
     );
 
     commentsList.appendChild(commentTag);
   }
-}
+};
 
 const handleLoadComment = async (cakeId, page) => {
   for (let i = 0; i < paginationBar.childElementCount; i++) {
-    paginationBar.children[i].classList.remove('active');
+    paginationBar.children[i].classList.remove("active");
 
     if (page - 1 === i) {
-      paginationBar.children[i].classList.add('active');
+      paginationBar.children[i].classList.add("active");
     }
   }
 
   const fetchUrl = `/comments?cake=${cakeId}&page=${page}`;
-  const response = await fetch(
-    fetchUrl,
-    {
-      method: 'GET'
-    }
-  );
+  const response = await fetch(fetchUrl, {
+    method: "GET",
+  });
 
   const { comments, pagination } = await response.json();
 
   renderComments(comments);
-}
+};
 
 (async () => {
   const url = new URL(location.href);
-  const cakeId = url.pathname.split('/cakes/')[1];
+  const cakeId = url.pathname.split("/cakes/")[1];
 
   const fetchUrl = `/comments?cake=${cakeId}`;
-  const response = await fetch(
-    fetchUrl,
-    {
-      method: 'GET'
-    }
-  );
+  const response = await fetch(fetchUrl, {
+    method: "GET",
+  });
 
   const { comments, pagination } = await response.json();
 
   renderComments(comments);
 
   for (let i = 0; i < pagination.num; i++) {
-    const literal = i === 0 ?
-      `<a class='active' onclick='handleLoadComment("${cakeId}", ${i + 1})'>${i + 1}</a>` :
-      `<a onclick='handleLoadComment("${cakeId}", ${i + 1})'>${i + 1}</a>`;
+    const literal =
+      i === 0
+        ? `<a class='active' onclick='handleLoadComment("${cakeId}", ${
+            i + 1
+          })'>${i + 1}</a>`
+        : `<a onclick='handleLoadComment("${cakeId}", ${i + 1})'>${i + 1}</a>`;
 
     const [number] = createElement(literal);
     paginationBar.appendChild(number);
@@ -82,26 +79,24 @@ const handleLoadComment = async (cakeId, page) => {
 const handleComment = async (cakeId) => {
   const url = `/comments?cake=${cakeId}`;
 
-  const response = await fetch(url,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        content: commentInput.value
-      })
-    }
-  );
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      content: commentInput.value,
+    }),
+  });
 
   const { comments, pagination } = await response.json();
 
   renderComments(comments);
 
-  commentInput.value = '';
+  commentInput.value = "";
 
   for (let i = 0; i < paginationBar.childElementCount; i++) {
-    paginationBar.children[i].classList.remove('active');
+    paginationBar.children[i].classList.remove("active");
   }
-  paginationBar.children[0].classList.add('active');
+  paginationBar.children[0].classList.add("active");
 };
