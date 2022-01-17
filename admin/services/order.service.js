9c268bf339cf9ab83e0ceab33dad3e0e72138a91
TLCRATEAM <64020797+TLCRATEAM@users.noreply.github.com>
2022-01-17 14:00:19 +0700

    Update order.service.js

--- admin/services/order.service.js
+++ admin/services/order.service.js
@@ -1,80 +1,68 @@
 const orderModel = require("../models/order.model");
-const orderDetailModel = require("../models/orderDetail.model");
+// const orderDetailModel = require("../models/orderDetail.model");
+const cakeModel = require("../models/cake.model");
+const userModel = require("../models/user.model");
+const categoryModel = require("../models/category.model");
 
-const getRetrieveOrders = async (start, end) => {
-  const pipeline = [];
-
-  pipeline.push({
-    $lookup: {
-      from: "orders",
-      localField: "order",
-      foreignField: "_id",
-      as: "order",
-    },
-    $unwind: "$order",
-    $lookup: {
-      from: "cakes",
-      localField: "cake",
-      foreignField: "_id",
-      as: "cake",
-    },
-    $unwind: "$cake",
-    $lookup: {
-      from: "categories",
-      localField: "category",
-      foreignField: "_id",
-      as: "category",
-    },
-    $unwind: "$category",
-    $lookup: {
-      from: "users",
-      localField: "orderer",
-      foreignField: "_id",
-      as: "orderer",
-    },
-    $unwind: "$orderer",
-  });
-
-  if (start !== undefined && start !== "") {
-    pipeline.push({
-      createdAt: {
-        $gte: new Date(start),
-      },
-    });
-  }
-  if (end !== undefined && end !== "") {
-    pipeline.push({
-      createdAt: {
-        $lt: new Date(end),
-      },
-    });
-  }
-
-  // const pipeline2 = [];
+const getRetrieveOrders = async (start, end, category) => {
   // if (start !== undefined && start !== "") {
-  //   pipeline2.push({
-  //     createdAt: {
-  //       $gte: new Date(start),
+  //   console.log(new Date(start));
+  //   pipeline.push({
+  //     $match: {
+  //       createdAt: {
+  //         $gte: new Date(start),
+  //       },
   //     },
   //   });
   // }
   // if (end !== undefined && end !== "") {
-  //   pipeline2.push({
-  //     createdAt: {
-  //       $lt: new Date(end),
+  //   console.log(new Date(end));
+  //   pipeline.push({
+  //     $match: {
+  //       createdAt: {
+  //         $lt: new Date(end),
+  //       },
   //     },
   //   });
   // }
+  if (start == undefined || start === "") {
+    start = new Date("2021-1-1");
+  }
+  if (end == undefined || end === "") {
+    end = new Date("2023-1-1");
+  }
   let orders;
   try {
-    orders = await orderDetailModel.aggregate(pipeline);
-    // .populate([{ path: "order" }, { path: "cake" }, { path: "category" }])
-    // .find(pipeline2);
+    if (category == undefined) {
+      orders = await orderModel
+        .find({ createdAt: { $gte: start, $lte: end } })
+        .populate([
+          {
+            path: "orderer",
+            model: "User",
+          },
+          {
+            path: "wares.cake",
+            model: "Cake",
+            populate: { path: "category", model: "Category" },
+          },
+        ]);
+      //   {
+      //     path: "wares.cake",
+      //     model: "Cake",
+      //   },
+      //   // {
+      //   //   path: "wares",
+      //   //   populate: { path: "wares.cake", model: "Cake" },
+      //   // },
+      // ]);
+    } else {
+      orders = [];
+    }
   } catch (error) {
     console.log(error);
     orders = [];
   }
-
   return orders;
 };
 
